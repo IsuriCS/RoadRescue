0d00c915518bdcf35adbf3247d9ca96c96c1af35
IsuriCS <isurics2@gmail.com>
2023-11-02 23:36:01 +0530

    Technician Dashboard

--- FrontEnd/src/assets/js/technician/technicianJs.js
+++ FrontEnd/src/assets/js/technician/technicianJs.js
@@ -25,3 +25,20 @@ $(".close").click(function () {
     $(".open").css("background-color","#9295A1");
     $(".close").css("background-color","#23A6F0");
 })
+
+
+
+const availability = () => {
+    const button1 = document.getElementById('button1');
+    const button2 = document.getElementById('button2');
+
+    button1.addEventListener('click', () => {
+        button1.classList.remove('activetog');
+        button2.classList.add('activetog');
+    });
+
+    button2.addEventListener('click', () => {
+        button2.classList.remove('activetog');
+        button1.classList.add('activetog');
+    });
+};
