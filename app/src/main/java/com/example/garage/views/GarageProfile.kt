7c76ae91ecc83bfa7a95258d3e9f601f7b38434f
TharinduDakshina <tharindudakshina527@gmail.com>
2024-01-07 01:33:53 +0530

    add garageProfile and update it

--- app/src/main/java/com/example/garage/views/GarageProfile.kt
+++ app/src/main/java/com/example/garage/views/GarageProfile.kt
@@ -1,23 +1,55 @@
 package com.example.garage.views
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.substring
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.garage.R
 
 @Composable
 fun garageProfile(
-
+    garageName:String,
+    garageId:String,
+    ownerName:String
 ){
     Column(
         modifier = defaultBackground,
@@ -27,36 +59,91 @@ fun garageProfile(
         Card (
             modifier = cardDefaultModifier,
             colors = CardDefaults.cardColors(containerColor = Color(0xFFB6C7E3)),
-            border = BorderStroke(width = 2.dp, Color.White)
+            border = BorderStroke(width = 2.dp, Color.White),
         ){
 
-            Row (modifier = Modifier.fillMaxWidth().background(Color.Red).fillMaxHeight(0.17f)){
+            Row (modifier = Modifier
+                .weight(1f)
+            ){
+                    // set profile pitcher
+                Card(
+                    shape = CircleShape,
+                    border = BorderStroke(width = 2.dp, color = Color.White),
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(8.dp)
+                        .fillMaxHeight(0.2f)
+                ) {
 
-                /*Column {
-                    //image
+                    Image(
+                        painter = painterResource(id = R.drawable.user_fill),
+                        contentDescription ="my pitcher",
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color(0xDFFFFFFF))
+                    )
                 }
 
-                Column {
-                    // details
-                }*/
 
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(0.dp, 14.dp, 2.dp)
+                ){
+
+                    Column {
+                        Text(
+                            text = buildAnnotatedString {
+                                withStyle(
+                                    style = SpanStyle(
+                                        fontSize = 50.sp
+                                    )
+                                ) {
+                                    append(garageName[0])
+                                }
+                                append(garageName.substring(1))
+
+                                withStyle(
+                                    style = SpanStyle(
+                                        fontSize = 50.sp
+                                    )
+                                ) {
+                                    append(" G")
+                                }
+                                append("arage")
+
+                            },
+                            color = Color(0xFF253555),
+                            fontWeight = FontWeight.ExtraBold,
+                            fontSize = 30.sp,
+                            fontFamily = FontFamily.Serif,
+                            textAlign = TextAlign.Justify,
+                            maxLines = 3,
+                            lineHeight = 50.sp
+                        )
+
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        Text(
+                            text = "$garageId",
+                            color = Color(0xB3000000),
+                            fontWeight = FontWeight.ExtraBold,
+                            fontSize=20.sp,
+                        )
+
+                        Text(
+                            text = "$ownerName",
+                            color = Color(0xB3000000),
+                            fontWeight = FontWeight.ExtraBold,
+                            fontSize=20.sp
+                        )
+                    }
+
+                }
             }
 
-            Row(modifier = Modifier.fillMaxWidth().background(Color.Yellow).fillMaxHeight(0.17f)) {
 
-            }
-
-            Row(modifier = Modifier.fillMaxWidth().background(Color.Green).fillMaxHeight(0.17f)) {
-
-            }
-
-            Row (modifier = Modifier.fillMaxWidth().background(Color.White).fillMaxHeight(0.17f)){
-
-            }
-
-            Row (modifier = Modifier.fillMaxWidth().background(Color.Magenta).fillMaxHeight(0.17f)){
-
-            }
 
         }
     }
--- app/src/main/java/com/example/garage/views/MainActivity.kt
+++ app/src/main/java/com/example/garage/views/MainActivity.kt
@@ -1,18 +1,58 @@
 package com.example.garage.views
 
+import android.graphics.drawable.Icon
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.garage.R
 import com.example.garage.ui.theme.GarageTheme
 import com.example.garage.views.Header
 import java.time.Period
 
 class MainActivity : ComponentActivity() {
+
+    private var selectedImageUri by mutableStateOf<Uri?>(null)
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            selectedImageUri = uri
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,10 +70,13 @@ class MainActivity : ComponentActivity() {
                         GarageDashboard("Tharindu Dakshina", Period.of(1,2,3),2500f)
                     }*/
 
-                    garageProfile()
+                    garageProfile("Saman","C-001","Thiran Sasanka")
+
                 }
             }
         }
     }
+
 }
+
 
