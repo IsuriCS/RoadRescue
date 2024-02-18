d6222b62f510481485801b1df51ed0c56526cf54
TharinduDakshina <tharindudakshina527@gmail.com>
2024-02-18 17:18:14 +0530

    connect backend and frount end

--- BackEnd/src/servlet/GarageServlet.java
+++ BackEnd/src/servlet/GarageServlet.java
@@ -1,16 +1,29 @@
 package servlet;
 
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-@WebServlet("/garage")
+import java.io.PrintWriter;
+import java.sql.Connection;
+
+@WebServlet(urlPatterns = "/garage")
 public class GarageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Mobile backend connected ");
+
+        System.out.println("mobile request is ok ");
+
+        /*PrintWriter writer=resp.getWriter();
+        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+        objectBuilder.add("status",200);
+        objectBuilder.add("message","Done");
+        objectBuilder.add("data","mobile app response is ok ");
+        writer.print(objectBuilder.build());*/
     }
 
     @Override
